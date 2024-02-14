8b6b0cb9183356c434f1bb2290a7006014d7b7a2
khpeet <kpeet@newrelic.com>
2024-02-14 08:48:59 -0500

    update target node versions

--- src/app.test.js
+++ src/app.test.js
@@ -5,7 +5,7 @@ const request = supertest(app);
 describe("/test endpoint", () => {
   it("should return a response", async () => {
     const response = await request.get("/test");
-    expect(response.status).toBe(302);
+    expect(response.status).toBe(200);
     expect(response.text).toBe("Test Success!");
   });
 });
